7d0b2758483d25f2221c6d5611063cc0b5c0e8e8
BeRiyal <128631774+BeRiyal@users.noreply.github.com>
2023-08-13 01:19:22 -0400

    Update index.js
    port from env

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoutes);
 
+const port = process.env.PORT || 3000;
 connectDB()
     .then(() => {
         console.log('MongoDB connected')
-        app.listen(3000, () => console.log('Server running on port 3000'));
+        app.listen(port, () => console.log('Server running on port 3000'));
     })
     .catch((err) => console.log(err));
