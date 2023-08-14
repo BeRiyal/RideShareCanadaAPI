8ff347ca1e8e396724a7ec26ed98248f6d0009f8
jinalpatel11 <Jpatel6625@conestogac.on.ca>
2023-08-14 09:21:28 -0500

    init car caontroller in index.js

--- server/index.js
+++ server/index.js
@@ -4,13 +4,14 @@ const bodyParser = require('body-parser');
 //local imports
 const connectDB = require('./db.js');
 const userRoutes = require('./Controllers/UserController.js');
-
+const carRoutes = require('./Controllers/CarController.js');
 //initialize express
 const app = express();
 
 //middleware
 app.use(bodyParser.json());
 app.use("/api/users", userRoutes);
+app.use("/api/cars", carRoutes);
 
 //connect to database
 const port = process.env.PORT || 3000;
